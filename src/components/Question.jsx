1d516764b5339524182ff9161ed1d17dcb35a29a
eleftheros <eleftheros_all@protonmail.com>
2022-01-07 09:47:12 +0200

    Add question props to Question

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,10 +1,10 @@
 import Answer from "./Answer";
 
-const Question = () => {
+const Question = ({ question }) => {
   return (
     <div className="text-center shadow-md select-none">
       <h2 className="bg-green-400 text-slate-200 text-4xl font-bold p-4">
-        Question
+        {question}
       </h2>
       <div className="flex flex-wrap justify-center">
         <Answer />
