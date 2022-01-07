45afb1fd1d1a5cc8ef987bd5eb65083888bb22e1
eleftheros <eleftheros_all@protonmail.com>
2022-01-07 13:17:41 +0200

    Add renderCongrats to Results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,11 +1,12 @@
+import { renderCongrats } from "../utils/helpers";
 import Button from "./Button";
 
 const Results = ({ correctAnswers, total, dispatch }) => {
   return (
     <div className="w-3/4 mx-auto min-h-screen shadow-xl flex justify-center text-slate-900">
       <div className="w-1/2 text-center">
-        <h2 className="bg-green-500 my-8 text-4xl font-bold p-4 rounded-xl">
-          Congratulations!
+        <h2 className="bg-purple-700 my-8 text-4xl font-bold p-4 rounded-xl text-slate-200">
+          {renderCongrats(correctAnswers, total)}
         </h2>
         <p className="text-lg my-4">
           You answered {correctAnswers}/{total}
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,3 +4,19 @@ export const shuffle = arr => {
     .sort((a, b) => a.sort - b.sort)
     .map(i => i.item);
 };
+
+export const renderCongrats = (correct, total) => {
+  const result = correct / total;
+
+  return result === 1
+    ? "Excellent!"
+    : result >= 0.8
+    ? "Very Good!"
+    : result >= 0.6
+    ? "Good!"
+    : result >= 0.4
+    ? "Quite Good!"
+    : result >= 0.2
+    ? "Not Good!"
+    : "Bad!";
+};
